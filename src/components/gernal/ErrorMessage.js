0394c69dafa0e8037e6430af150276a666731796
numanbaig_ <numansafi97@gmail.com>
2023-08-23 11:02:02 +0500

    fix deployment issues

--- src/components/gernal/ErrorMessage.js
+++ src/components/gernal/ErrorMessage.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Alert from '@mui/material/Alert';
+import React from "react";
+import Alert from "@mui/material/Alert";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
-
-export default function ErrorMessage({message}) {
-  const classes = useStyles();
-
+export default function ErrorMessage({ message }) {
   return (
-    <div className={classes.root}>
+    <div>
       <Alert severity="error">{message}</Alert>
     </div>
   );
